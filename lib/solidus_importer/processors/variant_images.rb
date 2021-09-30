465c10adbebf479114891799057a2f0dd820d41f
Vlad <vdanciu@yahoo.com>
2021-09-30 18:08:20 +0300

    Make multiple variants use one image if such specified in the file - only works with the extension solidus_asset_variant_options

--- lib/solidus_importer/processors/variant_images.rb
+++ lib/solidus_importer/processors/variant_images.rb
@@ -3,10 +3,15 @@
 module SolidusImporter
   module Processors
     class VariantImages < Base
+      @saved_images = {}
+
+      class << self
+        attr_accessor :saved_images  # provide class methods for reading/writing
+      end
+
       def call(context)
         @data = context.fetch(:data)
         return unless variant_image?
-
         variant = context.fetch(:variant)
         process_images(variant)
       end
@@ -15,7 +20,10 @@ module SolidusImporter
 
       def prepare_image
         attachment = @data['Variant Image']
-        image = Spree::Image.new
+        
+        if (self.class.saved_images.key?(attachment))
+          return self.class.saved_images[attachment]
+        end
 
         if attachment.match?(/^http/)
           io = URI.parse(attachment).open
@@ -28,7 +36,10 @@ module SolidusImporter
           raise URI::InvalidURIError, "bad URI #{attachment}"
         end
 
+        image = Spree::Image.new
         image.tap { |i| i.attachment = io }
+        self.class.saved_images[attachment] = image
+        image
       end
 
       def process_images(variant)
